3293dd500a5dd8bc9ca5067ff225b5d4ce9eee5c
Roman Kovalchyk <kovalchykrv@gmail.com>
2023-10-21 20:44:53 +0300

    Adding Active button

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,22 +2,24 @@ import React from 'react'
 import TodoListItem from '../TodoListItem';
 import "./styles.css"
 
-const TodoList = ({ list, onDeleteItem, onDoneItem }) => {
+const TodoList = ({ list, onDeleteItem, onDoneItem, onActiveItem }) => {
 
     //const [inputValue, setInputValue] = useState("")
 
     const renderTodoList = () => {
         const content = list.map((element, index) => {
-            const { name, done } = element
+            const { name, done, active } = element
            
             return (
                 <TodoListItem
                     name={name}
                     done={done}
+                    active={active}
                     id={index}
                     key={index}
                     onDeleteItem={onDeleteItem}
                     onDoneItem={onDoneItem}
+                    onActiveItem={onActiveItem}
                 />
             )
         })
